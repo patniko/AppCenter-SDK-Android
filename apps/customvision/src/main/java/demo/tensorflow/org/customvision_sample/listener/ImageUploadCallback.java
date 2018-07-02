2b8e4487284152970b3d234f38d9b6ea13a4a91b
Evgeniy Poletaikin <jekapolet777@gmail.com>
2018-07-02 16:16:29 +0300

    Remove customvision app

--- apps/customvision/src/main/java/demo/tensorflow/org/customvision_sample/listener/ImageUploadCallback.java
+++
@@ -1,6 +0,0 @@
-package demo.tensorflow.org.customvision_sample.listener;
-
-public interface ImageUploadCallback {
-    void onUploadSuccess();
-    void onUploadFailure(int responseCode, boolean duplicates);
-}
